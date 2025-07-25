cf2df466d561e9d58263b467d082578c8604ff88
sayabane <sayanta.banerjee@non.keysight.com>
2025-07-25 16:10:20 +0530

    Add the type function

--- variables.py
+++ variables.py
@@ -1,9 +1,12 @@
 Name = "Sayanta"
 Age = 27
 Profession = "Documentation Engineer"
+YOE = 4.2
 
 Age_new = Age + 1
 
 print(Name, Age, Profession)
 print ("My name is", Name)
 print("My age is", Age_new)
+print(type(YOE))
+print(type(Age_new))
