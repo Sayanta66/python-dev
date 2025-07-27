da48990354afebf35d21bc08dbfdea1c9afc9f51
sayabane <sayanta.banerjee@non.keysight.com>
2025-07-27 12:08:33 +0530

    add logical operators AND and OR

--- Logical_Operators.py
+++ Logical_Operators.py
@@ -2,3 +2,14 @@ a = 50
 b = 30
 print(not (a > b))
 print(not (a < b))
+
+a1 = False
+a2 = False
+print(a1 and a2)
+print(a1 or a2)
+
+num1 = 300
+num2 = 30
+
+print(num1 > 200 and num2 < 100)
+print(num1 or num2)
