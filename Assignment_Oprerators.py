7212813b60fe6085e6ba2391d061d980e993f417
sayabane <sayanta.banerjee@non.keysight.com>
2025-07-27 11:46:47 +0530

    add assignment operators

--- Assignment_Oprerators.py
+++ Assignment_Oprerators.py
@@ -2,3 +2,23 @@ sum = 25
 sum = sum + 5
 sum += 20
 print(sum)
+
+sum1 = 25
+sum1 -= 10
+print(sum1)
+
+sum2 = 25
+sum2 *= 3
+print(sum2)
+
+sum3 = 35
+sum3 /= 5
+print(sum3)
+
+sum4 = 30
+sum4 %= 5
+print(sum4)
+
+sum5 = 30
+sum5 **= 2
+print(sum5)
