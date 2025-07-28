58ece92bc54c271286220eeebdb3b3b503d44944
sayabane <sayanta.banerjee@non.keysight.com>
2025-07-28 18:19:49 +0530

    add a program to find out if a number is greater than or equal to than the other. Both are taken as input

--- Executing_input.py
+++ Executing_input.py
@@ -1,24 +1,24 @@
 # Program to execute an input
 
-# a = input("Enter the value of a:")
-# b = input("Enter the value of b:")
-# sum = a + b
-# print(sum)
+a = input("Enter the value of a:")
+b = input("Enter the value of b:")
+sum = a + b
+print(sum)
 
-# a1 = int(input("a: "))
-# a2 = int(input("b: "))
-# sum1 = a1+a2
-# print(sum1)
+a1 = int(input("a: "))
+a2 = int(input("b: "))
+sum1 = a1+a2
+print(sum1)
 
-# len = int(input("Enter the length of the square: "))
-# area = len * len
-# print("Area of the square is: ", area)
+len = int(input("Enter the length of the square: "))
+area = len * len
+print("Area of the square is: ", area)
 
-# a = float(input("Enter the first number: "))
-# b = float(input("Enter the second number: "))
-# sum = a+b
-# average = sum/2
-# print("Average of the numbers is: ", average)
+a = float(input("Enter the first number: "))
+b = float(input("Enter the second number: "))
+sum = a+b
+average = sum/2
+print("Average of the numbers is: ", average)
 
 a = int(input("a: "))
 b = int(input("b: "))
