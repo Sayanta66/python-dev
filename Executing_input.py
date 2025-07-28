a918d219cdab09c9068fd63ee646a3b9454870a9
sayabane <sayanta.banerjee@non.keysight.com>
2025-07-28 18:10:50 +0530

    add program to find area of square through input

--- Executing_input.py
+++ Executing_input.py
@@ -9,3 +9,7 @@ a1 = int(input("a: "))
 a2 = int(input("b: "))
 sum1 = a1+a2
 print(sum1)
+
+len = int(input("Enter the length of the square: "))
+area = len * len
+print("Area of the square is: ", area)
