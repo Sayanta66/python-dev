13e026f43f7bdef47dd5b7f5533750d4d0d3971f
sayabane <sayanta.banerjee@non.keysight.com>
2025-07-28 18:04:38 +0530

    addition of two values through input

--- Executing_input.py
+++ Executing_input.py
@@ -1,7 +1,11 @@
 # Program to execute an input
 
-
 a = input("Enter the value of a:")
 b = input("Enter the value of b:")
 sum = a + b
 print(sum)
+
+a1 = int(input("a: "))
+a2 = int(input("b: "))
+sum1 = a1+a2
+print(sum1)
