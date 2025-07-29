f064bbc738e3f02ec0095e41a784bb8d83463859
sayabane <sayanta.banerjee@non.keysight.com>
2025-07-29 17:38:14 +0530

    add str.count() that counts the occurences of a substring

--- String_functions.py
+++ String_functions.py
@@ -12,3 +12,7 @@ str3 = "I am aiming for Synopsys"
 print(str3.find("a"))
 print(str3.find(" "))
 print(str3.find("aiming"))
+
+str4 = "I have to get a 30 LPA job"
+print(str4.count("a"))
+print(str4.count("e"))
