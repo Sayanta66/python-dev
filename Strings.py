1f862d481f4aafc14d68e328afbaef0f53f69e4d
sayabane <sayanta.banerjee@non.keysight.com>
2025-07-29 15:14:04 +0530

    add tab space between a string

--- Strings.py
+++ Strings.py
@@ -3,3 +3,6 @@ print(len(str))
 
 str1 = "Python is a great language.\n It is widely used in ADS."
 print(str1)
+
+str2 = "40k is needed to stay in Gurgaon.\t 50k is needed to stay in Bangalore."
+print(str2)
