9fdf7d180deb1af5392bf89760d354416c9910df
sayabane <sayanta.banerjee@non.keysight.com>
2025-07-29 17:34:54 +0530

    add str.find

--- String_functions.py
+++ String_functions.py
@@ -7,3 +7,8 @@ print(str1.capitalize())
 
 str2 = "goggle"
 print(str2.replace("g", "o"), str2.capitalize())
+
+str3 = "I am aiming for Synopsys"
+print(str3.find("a"))
+print(str3.find(" "))
+print(str3.find("aiming"))
