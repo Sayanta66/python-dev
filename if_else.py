039136b0e44bce57b86ef758bc2867f2bf56e740
sayabane <sayanta.banerjee@non.keysight.com>
2025-07-30 16:18:27 +0530

    add if-else problems

--- if_else.py
+++ if_else.py
@@ -5,3 +5,11 @@ elif(40<marks<50):
     print("Candidate passed with Grace")
 else:
     print("Candidate Failed")
+
+n = int(input("Enter marks: "))
+if(n%2!=0 and 50<n<80):
+    print("Lucky Marks")
+elif(n%2==0 and n>=80):
+    print("Super Marks")
+else:
+    print("Unlucky Marks")
