a7084e19bfa592f84568216bf25c5f72b4d82d51
sayabane <sayanta.banerjee@non.keysight.com>
2025-07-30 15:32:48 +0530

    add if else conditional statement

---
+++ if_else.py
@@ -0,0 +1,7 @@
+marks = int(input("Enter obtained marks: "))
+if(marks >= 50):
+    print("Candidate Passed")
+elif(40<marks<50):
+    print("Candidate passed with Grace")
+else:
+    print("Candidate Failed")
