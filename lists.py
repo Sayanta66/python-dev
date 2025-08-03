993efc0bb33de782d34baadc8e306520d30e716e
sayabane <sayanta.banerjee@non.keysight.com>
2025-08-03 12:20:05 +0530

    add mutability in lists

--- lists.py
+++ lists.py
@@ -1,2 +1,12 @@
 age = [25, 36, 40, 55, 82, 90]
 print(len(age))
+age[0]=58
+age[1:3]=55,47,99
+print(age)
+
+str = "I am a good boy"
+print(str[0:7])
+
+Adita = ["Loves Shopping", "Loves Traveling", 25, 23000]
+Adita[1] = "Loves Sayanta"
+print(Adita)
