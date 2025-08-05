1b85a959451080e611532720808c66dfa77982b2
sayabane <sayanta.banerjee@non.keysight.com>
2025-08-05 15:56:26 +0530

    add list.sort(reverse=True) function that sorts any list in descending order

--- Lists2.py
+++ Lists2.py
@@ -2,3 +2,9 @@ marks = [55, 88, 75, 62, 50, 85]
 marks.append(65)
 marks.sort()
 print(marks)
+
+age = [22, 80, 65, 76, 99]
+"""This is the age of a 
+particular group of people."""
+age.sort(reverse=True)
+print(age)
