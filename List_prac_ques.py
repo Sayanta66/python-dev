dd756ff58ac6e39dea8cc9e36baffcabd643ed10
sayabane <sayanta.banerjee@non.keysight.com>
2025-08-07 16:31:47 +0530

    add a program to store the three movies in a list that comes as an input

---
+++ List_prac_ques.py
@@ -0,0 +1,5 @@
+a = str(input("Enter the name of the first movie: "))
+b = str(input("Enter the name of the second movie: "))
+c = str(input("Enter the name of the third movie: "))
+movies = [a, b, c]
+print(movies)
