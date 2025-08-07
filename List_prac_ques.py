98b7129496d33d879abd77dc8c8721506942bd0a
sayabane <sayanta.banerjee@non.keysight.com>
2025-08-07 16:45:03 +0530

    add program to check the palindrome in a list through conditional statements

--- List_prac_ques.py
+++ List_prac_ques.py
@@ -1,5 +1,15 @@
-a = str(input("Enter the name of the first movie: "))
-b = str(input("Enter the name of the second movie: "))
-c = str(input("Enter the name of the third movie: "))
-movies = [a, b, c]
-print(movies)
+# a = str(input("Enter the name of the first movie: "))
+# b = str(input("Enter the name of the second movie: "))
+# c = str(input("Enter the name of the third movie: "))
+# movies = [a, b, c]
+# print(movies)
+
+a1 = int(input("Enter a number: "))
+b1 = int(input("Enter a number: "))
+c1 = int(input("Enter a number: "))
+pal = [a1, b1, c1]
+pal2 = [c1, b1, a1]
+if(pal == pal2):
+    print("This is a palindrome")
+else:
+    print("This is not a palindrome")
