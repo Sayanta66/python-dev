dabf2622780f8e0b53adf3dfb87f413e49fdd22a
sayabane <sayanta.banerjee@non.keysight.com>
2025-08-07 16:58:50 +0530

    add the function to check the palindrome of the elements within the list using the copy method

--- List_prac_ques.py
+++ List_prac_ques.py
@@ -4,11 +4,24 @@
 # movies = [a, b, c]
 # print(movies)
 
+# a1 = int(input("Enter a number: "))
+# b1 = int(input("Enter a number: "))
+# c1 = int(input("Enter a number: "))
+# pal = [a1, b1, c1]
+# pal2 = [c1, b1, a1]
+# if(pal == pal2):
+#     print("This is a palindrome")
+# else:
+#     print("This is not a palindrome")
+
 a1 = int(input("Enter a number: "))
-b1 = int(input("Enter a number: "))
-c1 = int(input("Enter a number: "))
-pal = [a1, b1, c1]
-pal2 = [c1, b1, a1]
+b1 = str(input("Enter a string: "))
+c1 = str(input("Enter another string: "))
+d1 = int(input("Enter a number: "))
+pal = [a1, b1, c1, d1]
+pal1 = pal.copy()
+pal2 = pal1[::-1]
+print(pal1, pal2)
 if(pal == pal2):
     print("This is a palindrome")
 else:
