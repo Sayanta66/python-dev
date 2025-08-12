5fb96a63f7fa1211c2054b0f1e13d1d9157b41fb
sayabane <sayanta.banerjee@non.keysight.com>
2025-08-12 15:56:07 +0530

    add lists and tuples in a dictionary

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,6 +1,8 @@
 student_info = {
     "firstname" : "Sayanta",
     "lastname" : "Banerjee",
-    "rollno." : 36
+    "rollno." : 36,
+    "subjects" : ["maths", "physics", "chemistry"],
+    "Sections" : ("A", "B")
 }
 print(student_info)
