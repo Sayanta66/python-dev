1a500b98f4373aeb09729b760e66a501ca58ff29
sayabane <sayanta.banerjee@non.keysight.com>
2025-08-19 16:41:58 +0530

    set prints the collection of data but ignores the duplicates

--- Sets.py
+++ Sets.py
@@ -2,3 +2,6 @@ players = {"Messi", "Ronaldo", "Neymar", "Aguero"}
 print(players)
 print(type(players))
 print(len(players))
+
+teams = {"Barcelona", "Real Madrid", "Manchester", "Manchester", "Chelsea"}
+print(teams)
