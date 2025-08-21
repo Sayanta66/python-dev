d336893e6f4c0f45a79eccc51cd0a83b0f4e556d
sayabane <sayanta.banerjee@non.keysight.com>
2025-08-21 12:55:14 +0530

    add a new set

--- Sets.py
+++ Sets.py
@@ -14,3 +14,8 @@ teams2.remove("Python is great")
 print(teams2)
 teams2.add((1,2,3,4))
 print(teams2)
+
+set_new={1, 2, 5, 5, 9, 2, "Good", "Nice"}
+print(set_new)
+# set1 = set_new.clear()
+# print(set1)
